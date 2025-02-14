f9d1c914ad4d04392825304e1189a21a69e51b87
whuang45 <=>
2025-02-13 18:03:34 -0800

    Added Specifications to Card and CardList Class

--- src/main/model/Card.java
+++ src/main/model/Card.java
@@ -1,5 +1,7 @@
 package model;
 
+// Represents a card having with its name, current condition, card rarity, the type of card, 
+// the price represented by an integer, current stock status of the card and finally the quantity of the card. 
 public class Card {
     private String condition;
     private String rarity;
@@ -10,7 +12,8 @@ public class Card {
     private int quantity;
 
  
-
+    // EFFECTS: Create a new card based on the its condition, rarity, name, type and price. The newly created
+    // card is not sold and the initial quantity is set to 1. 
     public Card (String condition, String rarity, String name, String type, int price) {
         this.condition = condition; 
         this.rarity = rarity; 
@@ -22,34 +25,44 @@ public class Card {
         
     }
 
+    // EFFECTS: Concatenates relavent informations information of a card into one string 
     public String readCardInfo() {
         String cardinfo = name + ", " + type + ", " + rarity + ", " + condition + ", " + "$" + Integer.toString(price);
         return cardinfo;         
     }
+
+    // MODIFIES: this
+    // EFFECTS: increase the quantity of card by one
     public void increaseQuantity() {
         this.quantity++;
     }
     
+    // EFFECTS: returns the condition of card 
     public String getCondition() {
         return condition;
     }
 
+    // EFFECTS: returns the rarity of card
     public String getRarity() {
         return rarity;
     }
 
+    // EFFECTS: returns the name of card
     public String getName() {
         return name;
     }
 
+    // EFFECTS: returns the type of card
     public String getType() {
         return type;
     }
 
+    // EFFECTS: returns the price of card
     public int getPrice() {
         return price;
     }
 
+    // EFFECTS: returns the sold status of card
     public boolean getSold() {
         return sold;
     }
---
+++ src/main/model/CardList.java
@@ -0,0 +1,50 @@
+package model;
+
+import java.util.ArrayList;
+import model.Card;
+
+public class CardList {
+    private ArrayList<Card> mybinder; 
+
+    public CardList(){
+        mybinder = new ArrayList<Card>();
+    }
+    
+    // MODIFIES: this, Card
+    // EFFECTS: Add a card into the owner's binder, if the added card's name already exsist in the collection, increase the card's 
+    // quantity by one instead of mybinder.add() and return false.
+    public Boolean addCard(Card newcard) { 
+        if (mybinder.contains(newcard)) {
+            newcard.increaseQuantity();
+            return false;
+        }
+
+        else {
+            mybinder.add(newcard);
+            return true; 
+        }
+    }
+
+    // EFFECTS: Search mybinder for a card, return the card if the card is found, return null if not found.
+    public Card searchCard(String cardname) { 
+
+        for (Card targetcard : mybinder) {
+            if (targetcard.getName().equals(cardname)) { 
+                return targetcard; 
+            }
+        }
+        return null;
+    }
+    
+
+    //EFFECTS: Return the value of the list of cards names given. 
+    public int calculateValue (ArrayList<String> loc) {
+        return 0;
+    }
+
+    //EFFECTS: returns the user's collection.
+    public ArrayList<Card> getMyBinder() {
+        return mybinder;
+    }
+    
+}
