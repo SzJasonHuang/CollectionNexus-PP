0bec51934841e0196dd4ad8c610789a7c4d51d46
whuang45 <=>
2025-02-15 12:08:35 -0800

    Completed Implementation for CardWishList Class

--- src/main/model/CardWishList.java
+++ src/main/model/CardWishList.java
@@ -8,19 +8,26 @@ public class CardWishList {
 
     // Creates a wishlist of cards with no cards in it
     public CardWishList() {
-        
+        mywishlist = new ArrayList<Card>();
     }
 
-    //MODIFIES: this
-    //EFFECTS: adds a single card to the user's wishlist
+    //MODIFIES: this, Card
+    //EFFECTS: adds a single card to the user's wishlist, a card's quantity is increased
+    //by one if it had been added twice and it would not be added to the wishlist. 
     public void addCardToWishList(Card wishcard) {
+        mywishlist.add(wishcard);
 
     }
 
+    //REQUIRES: mywishlist must contain the card trying to be removed
     //MODIFIES: this
     //EFFECTS: removes a single card from the user's wishlist
     public void removeCardFromWishList(String wishcard) {
-
+        for(Card indicard : mywishlist){
+            if(indicard.getName() == wishcard){
+                mywishlist.remove(indicard);
+            }
+        }
     }
 
     //EFFECTS: returns user's wishlist
