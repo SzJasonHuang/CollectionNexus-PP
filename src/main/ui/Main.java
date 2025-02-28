d3ffa6e7261c64f5d9075d251afedc84546ead7b
whuang45 <=>
2025-02-27 21:02:22 -0800

    Added Try Catch to main

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,8 +1,15 @@
 package ui;
 
+import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        new CollectionNexusApp();
+        try {
+            new CollectionNexusApp();
+        } catch (FileNotFoundException e) {
+            System.out.println("Unable to run application: file not found");
+        }
+
     }
+
 }
