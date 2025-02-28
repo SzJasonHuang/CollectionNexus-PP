d02fbc6f911086d28c6329062ce71294b8789fd2
whuang45 <=>
2025-02-27 21:02:43 -0800

    Finished implementation for saving and loading data in ui

--- src/main/ui/CollectionNexusApp.java
+++ src/main/ui/CollectionNexusApp.java
@@ -5,18 +5,31 @@ import model.Card;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import persistence.*;
 
 //Code below took inspiration from TellerApp 
 // 
 
 //Connection Nexus App
 public class CollectionNexusApp {
+    private static final String JSON_STORE_binder = "./data/binder.json";
+    private static final String JSON_STORE_wishlist = "./data/wishlist.json";
     private CardList binder;
     private CardList wishlist;
     private Scanner input;
+    private JsonWriter jsonWriter;
+    private JsonWriter jsonWriter1;
+    private JsonReader jsonReader;
+    private JsonReader jsonReader1;
 
     // EFFECTS: Runs the CollectionNexus App
-    public CollectionNexusApp() {
+    public CollectionNexusApp() throws FileNotFoundException {
+        jsonReader = new JsonReader(JSON_STORE_binder);
+        jsonReader1 = new JsonReader(JSON_STORE_wishlist);
+        jsonWriter = new JsonWriter(JSON_STORE_binder);
+        jsonWriter1 = new JsonWriter(JSON_STORE_wishlist);
         runApp();
     }
 
@@ -142,7 +155,7 @@ public class CollectionNexusApp {
         while (!terminate1) {
             displayBinderMenu();
             command1 = input.nextLine().trim();
-            if (command1.equals("d")) {
+            if (command1.equals("f")) {
                 terminate1 = true;
             } else {
                 processCommandForBinder(command1);
@@ -157,11 +170,14 @@ public class CollectionNexusApp {
         System.out.println("a. Search for a card in my binder");
         System.out.println("b. Value Calculator");
         System.out.println("c. Get current collection as a list of card");
-        System.out.println("d. Back to main menu");
+        System.out.println("d. Save my binder to file");
+        System.out.println("e. Load my binder to file");
+        System.out.println("f. Back to main menu");
     }
 
     // MODIFIES: this
-    // EFFECTS: processes user's command in the binder menu, based on user's selection of alphabet
+    // EFFECTS: processes user's command in the binder menu, based on user's
+    // selection of alphabet
     public void processCommandForBinder(String command) {
         if (command.equals("a")) {
             searchCardInBinder();
@@ -169,12 +185,17 @@ public class CollectionNexusApp {
             calculateValueInBinder();
         } else if (command.equals("c")) {
             getListOfCardInCollection();
+        } else if (command.equals("d")) {
+            saveBinder();
+        } else if (command.equals("e")) {
+            loadBinder();
         } else {
             System.out.println("Selection is not valid, please enter the alphabet that corresponds the command");
         }
     }
 
-    //EFFECTS: Given name of card, search user's binder for a card that matches the name and
+    // EFFECTS: Given name of card, search user's binder for a card that matches the
+    // name and
     // displays the details to the owner if found.
     public void searchCardInBinder() {
         String target = null;
@@ -200,34 +221,36 @@ public class CollectionNexusApp {
         }
     }
 
-    //EFFECTS: Calculate the total value of the input list if cards are in user's binder.
+    // EFFECTS: Calculate the total value of the input list if cards are in user's
+    // binder.
+
     public void calculateValueInBinder() {
-        boolean stillcalculate = true;
-        while (stillcalculate) {
+        while (true) {
             System.out.println("Enter name of cards separated by spaces: ");
-            String line = input.nextLine();
-            ArrayList<String> havecards = new ArrayList<>(Arrays.asList(line.split(" ")));
-            int totalvalueforuser = binder.calculateValue(havecards);
-            System.out.println("The total value of your collection is " + totalvalueforuser + " Dollars");
-            System.out.println("Do you still need to calculate more card values? yes/no");
-            String answer = input.next();
-            if (answer.equalsIgnoreCase("yes")) {
-                stillcalculate = true;
-            } else if (answer.equalsIgnoreCase("no")) {
-                stillcalculate = false;
-            } else {
-                System.out.println("Command invalid, please type yes/no for your answer");
+            String line = input.nextLine().trim();
+            if (line.isEmpty()) {
+                System.out.println("No cards entered, please try again.");
+                continue;
             }
+
+            int totalValue = binder.calculateValue(new ArrayList<>(Arrays.asList(line.split(" "))));
+            System.out.println("The total value of your selected card is " + totalValue + " Dollars");
+
+            System.out.println("Do you still need to calculate more card values? yes/no");
+            if (!input.next().trim().equalsIgnoreCase("yes")) {
+                break;
+            }
+            input.nextLine();
         }
     }
 
-    //EFFECTS: Displays all card's information in the owner's binder
+    // EFFECTS: Displays all card's information in the owner's binder
     public void getListOfCardInCollection() {
         for (Card indi : binder.getMyBinder()) {
             System.out.println(indi.readCardInfo());
         }
     }
-    
+
     // MODIFIES: this
     // EFFECTS: Displays the Current user's wishlist and give
     // a list of options to do with it.
@@ -237,7 +260,7 @@ public class CollectionNexusApp {
         while (!terminate1) {
             displayWishListMenu();
             command1 = input.nextLine().trim();
-            if (command1.equals("d")) {
+            if (command1.equals("f")) {
                 terminate1 = true;
             } else {
                 processCommandForWishList(command1);
@@ -252,10 +275,12 @@ public class CollectionNexusApp {
         System.out.println("a. Add a new card to the wishlist");
         System.out.println("b. Remove a card from the wishlist");
         System.out.println("c. View my wishlist");
-        System.out.println("d. Back to main menu");
+        System.out.println("d. Save my wishlist to file");
+        System.out.println("e. Load my wishlist to file");
+        System.out.println("f. Back to main menu");
     }
 
-    // EFFECTS: Processes the command from user given from the wishlist menu. 
+    // EFFECTS: Processes the command from user given from the wishlist menu.
     public void processCommandForWishList(String command) {
         if (command.equals("a")) {
             addCardToWishList();
@@ -263,6 +288,10 @@ public class CollectionNexusApp {
             removeCardFromWishList();
         } else if (command.equals("c")) {
             printCurrentLists("wishlist");
+        } else if (command.equals("d")) {
+            saveWishlist();
+        } else if (command.equals("e")) {
+            loadWishlist();
         } else {
             System.out.println("Selection is not valid, please enter the alphabet that corresponds the command");
         }
@@ -299,6 +328,53 @@ public class CollectionNexusApp {
                 System.out.println(card.readCardInfo());
             }
         }
+    }
 
+    // EFFECTS: saves binder to file
+    public void saveBinder() {
+        try {
+            jsonWriter.open();
+            jsonWriter.write(binder);
+            jsonWriter.close();
+            System.out.println("Saved Binder to " + JSON_STORE_binder);
+        } catch (FileNotFoundException e) {
+            System.out.println("Unable to write to file: " + JSON_STORE_binder);
+        }
+    }
+
+    // EFFECTS: saves wishlist to file
+    public void saveWishlist() {
+        try {
+            jsonWriter1.open();
+            jsonWriter1.write(wishlist);
+            jsonWriter1.close();
+            System.out.println("Saved Wishlist to " + JSON_STORE_wishlist);
+        } catch (FileNotFoundException e) {
+            System.out.println("Unable to write to file: " + JSON_STORE_wishlist);
+        }
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: loads binder to file
+    public void loadBinder() {
+        try {
+            binder = jsonReader.read();
+            System.out.println("Loaded Binder from" + JSON_STORE_binder);
+        } catch (IOException e) {
+            System.out.println("Unable to write to file: " + JSON_STORE_binder);
+        }
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: loads wishlist to file
+    public void loadWishlist() {
+        try {
+            wishlist = jsonReader1.read();
+            System.out.println("Loaded Wishlist from" + JSON_STORE_wishlist);
+        } catch (IOException e) {
+            System.out.println("Unable to write to file: " + JSON_STORE_wishlist);
+        }
     }
 }
